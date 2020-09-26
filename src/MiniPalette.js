078fad4a59a4a7352568d81fbc0a8427a18833ea
Yugesh Kekre <kekre.dev@gmail.com>
2020-09-26 17:50:16 +0530

    Add and style mini palettes for index page

---
+++ src/MiniPalette.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+
+const styles = {
+  colorbox: {},
+  container: {},
+
+  root: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#fffafa",
+    cursor: "pointer",
+
+    "& $container": {
+      display: "flex",
+      flexWrap: "wrap",
+      height: "76%",
+
+      "& $colorbox": {
+        height: "25%",
+        width: "20%",
+      },
+    },
+  },
+  text: {
+    height: "24%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    "& h3": {
+      fontFamily: "Roboto, sans-serif",
+      fontWeight: "500",
+      color: "rgba(0, 0, 0, 0.54)",
+      marginLeft: "1%",
+      marginRight: "1%",
+    },
+    "& span": {
+      fontSize: "1.5rem",
+      marginRight: "1%",
+      paddingRight: "1%",
+    },
+  },
+};
+function MiniPalette(props) {
+  const { classes } = props;
+  const { colors, emoji, paletteName } = props.palette;
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.container}>
+        {colors.map((c) => {
+          return (
+            <div
+              className={classes.colorbox}
+              style={{ background: c.color }}
+              key={c.name}
+            ></div>
+          );
+        })}
+      </div>
+      <div className={classes.text}>
+        <h3 className="name">{paletteName}</h3>
+        <span className="emoji">{emoji}</span>
+      </div>
+    </div>
+  );
+}
+
+export default withStyles(styles)(MiniPalette);
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,27 +1,92 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import MiniPalette from "./MiniPalette";
+import { withStyles } from "@material-ui/styles";
 
+const styles = {
+  paletteList: {
+    height: "100vh",
+    backgroundColor: "#00a8ff66",
+  },
+
+  nav: {
+    display: "flex",
+    width: "60%",
+    margin: "0 auto",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: "2%",
+
+    "& span": {
+      fontFamily: "'Pacifico', cursive",
+      fontWeight: "200",
+      fontSize: "2rem",
+      marginLeft: "1%",
+    },
+
+    "& a": {
+      fontFamily: "Roboto, sans-serif",
+      fontWeight: 300,
+      padding: 5,
+      color: "rgba(0, 0, 0, 0.54)",
+      fontSize: "1.1rem",
+      marginRight: "5%",
+    },
+  },
+
+  palettesContainer: {
+    display: "flex",
+    height: "80%",
+    width: "60%",
+    margin: "0 auto",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+
+  eachPalette: {
+    width: "30%",
+    height: "25%",
+    margin: "1% 1%",
+
+    "& a": {
+      textDecoration: "none",
+    },
+  },
+};
 class PaletteList extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-  render() {
-    const palettes = this.props.palettes;
-    return (
-      <div className="PaletteList">
-        <h1>List of palettes</h1>
 
-        {palettes.map((p) => {
-          return (
-            <p>
-              <Link to={`/palettes/` + p.id}>{p.paletteName}</Link>{" "}
-            </p>
-          );
-        })}
+  render() {
+    const { classes, palettes } = this.props;
+    return (
+      <div className={classes.paletteList}>
+        <div className={classes.nav}>
+          <span className={classes.navTitle}>Palettes Picker</span>
+          <Link to="/" className={classes.navButton}>
+            Create Palette
+          </Link>
+        </div>
+
+        <div className={classes.palettesContainer}>
+          {palettes.map((p) => {
+            return (
+              <div className={classes.eachPalette} key={p.id}>
+                <Link to={`/palettes/` + p.id}>
+                  <MiniPalette palette={p} />
+                </Link>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
